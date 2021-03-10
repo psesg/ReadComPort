b5bfb2791acf8334331cb954f86204357765a4cc
Serg <pseesp@mail.ru>
2021-03-10 12:53:30 +0300

    Initial commit

---
+++ readcomport.py
@@ -0,0 +1,5 @@
+# https://pythonhosted.org/pyserial/shortintro.html#opening-serial-ports
+import serial
+ser = serial.Serial('COM1', 19200, timeout=1,   parity=serial.PARITY_NONE)
+
+print("connected to: " + ser.portstr)

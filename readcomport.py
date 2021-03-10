df7fbe63d2d6ba710ac87c91437e48da3a4f7ec5
Serg <pseesp@mail.ru>
2021-03-11 00:14:17 +0300

    some

--- readcomport.py
+++ readcomport.py
@@ -4,15 +4,36 @@ import serial.tools.list_ports as port_list
 ports = list(port_list.comports())
 for p in ports:
     print ("port='{}'".format(p))
-
-ser = serial.Serial('COM3', 9600, timeout=1,   parity=serial.PARITY_NONE)
-
-print("connected to: " + ser.portstr)
+chunk = 1024
+#ser1 = serial.Serial('COM1', 9600, timeout=1,   parity=serial.PARITY_NONE)
+ser2 = serial.Serial('COM2', 9600, timeout=None,   parity=serial.PARITY_NONE)
+#print("connected to: " + ser1.name)
+print("connected to: " + ser2.name)
 command = b'\x30\x30\x30\x30\x31\x39\x31\x39'
-ser.write(command)
-s = ser.read(8)
+#ser1.write(command)
+
+s = ser2.read(8)
+
+sint = int(s)
 print(s)
-print("read={}".format(s))
+print("need_read={}".format(sint))
+#file = open("leaked.rar", "wb")
+n = sint // chunk # chunk
+m = sint % chunk # reminder
+print(n,m)
 # copy /b size.txt+astropy.rar itog.bin
+# copy itog.bin com1 /b
+#while n > 0:
+#    print(n)
+#    s = ser2.read(chunk)
+#   file.write(s)
+#    n = n - 1
+
+#print(sint)
+#s = ser2.read(sint)
+#ser2.close()
+#print(s)
+#file.write(s)
+#file.close()
 
 

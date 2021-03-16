00c15db6c4bd3d884b7a8fa7b253d62368a1adab
pseSg <pseesp@mail.ru>
2021-03-16 10:23:24 +0300

    optimized calculate file size from header some change

--- readcomport.py
+++ readcomport.py
@@ -13,8 +13,11 @@ else:
     for p in ports:
         print ("\tport='{}'".format(p))
 
-ser = serial.Serial('COM4', 19200, timeout=None,   parity=serial.PARITY_NONE) #, xonxoff=False, rtscts=True,dsrdtr=True)
+ser = serial.Serial('COM6', baudrate=19200, timeout=None,   parity=serial.PARITY_NONE, dsrdtr=True,  rtscts=True) #, xonxoff=False, rtscts=True,dsrdtr=True)
 print("Will wait connection and data on: " + ser.name)
+print("baudrate={} parity={} bytesize={} stopbits={} timeout={}".format(ser.baudrate,ser.parity,ser.bytesize,ser.stopbits,ser.timeout))
+print("xonxoff={} sync cts={} sync dsr={} dsrdtr={} rtscts={}".format(ser.xonxoff,ser.cts,ser.dsr,ser.dsrdtr,ser.rtscts))
+print("dtr={} rts={}".format(ser.dtr,ser.rts ))
 
 need2read = 0
 k = 8
@@ -27,6 +30,7 @@ while k > 0:
         #print("Head", k, int(l), type(l))
         k = k - 1
         if k == 0:
+            print(ar_bytes)
             need2read = int(ar_bytes.decode("ansi"))
             #print(need2read)
         if k == 0 and need2read > 0:
@@ -61,3 +65,7 @@ print ("File size: {} elapsed time:  {} s ({} kB/ses)".format(need2read, elapse.
 # mode com3 baud=115200 data=8 parity=n xon=off odsr=on octs=on dtr=on rts=on to=off
 # mode com3 baud=19200 parity=n data=8 stop=1 to=off xon=off odsr=off octs=off dtr=on rts=on idsr=off
 # copy itog.bin /b com3 /b
+
+# virt ser = serial.Serial('COM6', baudrate=19200, timeout=None,   parity=serial.PARITY_NONE, dsrdtr=True,  rtscts=True)
+# virt mode com4 baud=19200 parity=n data=8 stop=1 to=off xon=off odsr=on octs=off dtr=on rts=on idsr=off octs=on
+# virt
